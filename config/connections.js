2bd07bfd95854c883f6eff44b06dd992e566b72e
emyeunuoc <namko317@gmail.com>
2017-07-20 03:11:26 +0700

    sa

--- config/connections.js
+++ config/connections.js
@@ -44,7 +44,7 @@ module.exports.connections = {
     adapter: 'sails-mysql',
     host: 'localhost',
     user: 'root', //optional
-    password: 'abc123', //optional
+    password: 'Anhyeuem.123', //optional
     database: 'yuki' //optional
   },
 
